93e5dc261c85dbc24a524dd21d2de5be58dca743
thanhtan290598 <77422806+thanhtan290598@users.noreply.github.com>
2021-02-20 00:30:32 +0700

    commit identity card api - TANNT

---
+++ ImportExportManagementAPI/Controllers/IdentityCardsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ImportExportManagement_API;
+using ImportExportManagement_API.Models;
+using ImportExportManagementAPI.Repositories;
+
+namespace ImportExportManagementAPI.Controllers
+{
+    [Route("api/card")]
+    [ApiController]
+    public class IdentityCardsController : ControllerBase
+    {
+        private readonly IdentityCardRepository _repo;
+
+        public IdentityCardsController(IEDbContext context)
+        {
+            _repo = new IdentityCardRepository();
+        }
+
+        // GET: api/IdentityCards
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<IdentityCard>>> GetIdentityCard()
+        {
+            return await _repo.GetAllAsync();
+        }
+
+        // GET: api/IdentityCards/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IdentityCard>> GetIdentityCard(int id)
+        {
+            var identityCard = await _repo.GetByIDAsync(id);
+
+            if (identityCard == null)
+            {
+                return NotFound();
+            }
+
+            return identityCard;
+        }
+
+        // PUT: api/IdentityCards/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIdentityCard(int id, IdentityCard identityCard)
+        {
+            if (id != identityCard.IdentityCardId)
+            {
+                return BadRequest();
+            }
+
+            _repo.Update(identityCard);
+
+            try
+            {
+                await _repo.SaveAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_repo.Exist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/IdentityCards
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<IdentityCard>> PostIdentityCard(IdentityCard identityCard)
+        {
+            _repo.Insert(identityCard);
+            await _repo.SaveAsync();
+
+            return CreatedAtAction("GetIdentityCard", new { id = identityCard.IdentityCardId }, identityCard);
+        }
+
+        // DELETE: api/IdentityCards/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIdentityCard(int id)
+        {
+            var identityCard = await _repo.GetByIDAsync(id);
+            if (identityCard == null)
+            {
+                return NotFound();
+            }
+
+            _repo.DeleteIdentityCard(identityCard);
+            await _repo.SaveAsync();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ ImportExportManagementAPI/Repositories/IdentityCardRepository.cs
@@ -0,0 +1,57 @@
+﻿using ImportExportManagement_API.Models;
+using ImportExportManagement_API.Repositories;
+using ImportExportManagementAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportExportManagementAPI.Repositories
+{
+    public class IdentityCardRepository : BaseRepository<IdentityCard>
+    {
+        public async ValueTask<List<IdentityCard>> GetAllAsync(IdentityCardFilter filter)
+        {
+            List<IdentityCard> IdentityCards = new List<IdentityCard>();
+            IQueryable<IdentityCard> rawData = null;
+            rawData = _dbSet.Where(p => p.IdentityCardStatus == IdentityCardStatus.Active);
+            IdentityCards =  DoFilter(filter, rawData);
+            //schedules = _dbSet.ToList();
+            return IdentityCards;
+        }
+
+        private  List<IdentityCard> DoFilter(IdentityCardFilter filter, IQueryable<IdentityCard> queryable)
+        {
+            if (filter.Name != null && filter.Name.Length > 0)
+            {
+                queryable = queryable.Where(p => p.Partner.DisplayName.Contains(filter.Name));
+            }
+            if (filter.Role != null && filter.Role.Length > 0)
+            {
+                queryable = queryable.Where(p => p.Partner.Account.Role.RoleName.Contains(filter.Role));
+            }
+            if (filter.Status != null && filter.Status.Length > 0)
+            {
+                queryable = queryable.Where(p => p.IdentityCardStatus.Equals(filter.Status));
+            }
+            return  queryable.ToList();
+        }
+
+        public List<IdentityCard> GetIdentityCards()
+        {
+            List<IdentityCard> IdentityCards = null;
+            IdentityCards = _dbSet.Where(p => p.IdentityCardStatus == IdentityCardStatus.Active).ToList();
+            return IdentityCards;
+        }
+        public bool Exist(int id)
+        {
+            return _dbSet.Any(e => e.IdentityCardId == id);
+        }
+
+        public void DeleteIdentityCard(IdentityCard IdentityCard)
+        {
+            IdentityCard.IdentityCardStatus = IdentityCardStatus.Block;
+            Update(IdentityCard);
+        }
+    }
+}
