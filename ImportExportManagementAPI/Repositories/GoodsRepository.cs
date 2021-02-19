9f57818e356ffaf3ff8bb99c7d2cf4ad7f42c255
thanhtan290598 <77422806+thanhtan290598@users.noreply.github.com>
2021-02-19 21:19:19 +0700

    commit goods api

---
+++ ImportExportManagementAPI/Repositories/GoodsRepository.cs
@@ -0,0 +1,29 @@
+﻿using ImportExportManagement_API.Models;
+using ImportExportManagement_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportExportManagementAPI.Repositories
+{
+    public class GoodsRepository : BaseRepository<Goods>
+    {
+        public List<Goods> GetGoods()
+        {
+            List<Goods> goods = null;
+            goods = _dbSet.ToList();
+            return goods;
+        }
+        public bool Exist(int id)
+        {
+            return _dbSet.Any(e => e.GoodsId == id);
+        }
+
+        public void DeleteGoods(Goods  goods)
+        {
+           // partner.PartnerStatus = PartnerStatus.Block;
+            Update(goods);
+        }
+    }
+}
