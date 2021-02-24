a39ce93550ddd4d7dc65a26af5439b114e4e10b7
tantmse130742 <77141426+tantmse130742@users.noreply.github.com>
2021-02-25 05:07:34 +0700

    edit transaction api

--- ImportExportManagementAPI/Repositories/TransactionRepository.cs
+++ ImportExportManagementAPI/Repositories/TransactionRepository.cs
@@ -13,7 +13,7 @@ namespace ImportExportManagementAPI.Repositories
     {
         public async ValueTask<List<Transaction>> GetAllAsync(TransactionFilter filter)
         {
-            List<Transaction> listTransaction = new List<Transaction>();
+            List<Transaction> listTransaction = null;
             IQueryable<Transaction> rawData = null;
             rawData = _dbSet;
             listTransaction = await DoFilter(filter, rawData);
@@ -22,14 +22,19 @@ namespace ImportExportManagementAPI.Repositories
 
         private async Task<List<Transaction>> DoFilter(TransactionFilter filter, IQueryable<Transaction> queryable)
         {
-            if (filter.partnerName != null && filter.partnerName.Length > 0)
+            if (filter.PartnerName != null && filter.PartnerName.Length > 0)
             {
-                queryable = queryable.Where(p => p.IdentityCard.Partner.DisplayName.Contains(filter.partnerName));
+                queryable = queryable.Where(p => p.IdentityCard.Partner.DisplayName.Contains(filter.PartnerName));
             }
-            if (DateTime.TryParse(filter.dateCreate, out DateTime date))
+            if (DateTime.TryParse(filter.DateCreate, out DateTime date) && filter.DateCreate != null)
             {
-                DateTime dateCreate = DateTime.Parse(filter.dateCreate);
-                queryable = queryable.Where(p => p.TimeIn == dateCreate);
+                DateTime dateCreate = DateTime.Parse(filter.DateCreate);
+                queryable = queryable.Where(p => DateTime.Compare(p.TimeIn, dateCreate) == 0);
+            }
+            if (Enum.TryParse(filter.TransactionType, out TransactionType transactionType))
+            {
+                TransactionType type = (TransactionType)Enum.Parse(typeof(TransactionType), filter.TransactionType);
+                queryable = queryable.Where(p => p.TransactionType == type);
             }
             return await queryable.ToListAsync();
         }
