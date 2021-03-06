f764a0eeac724151a463c4f3a63e6de925463ad1
thanhtan290598 <77422806+thanhtan290598@users.noreply.github.com>
2021-03-06 09:59:12 +0700

    tannt - update api account login

---
+++ ImportExportManagementAPI/Controllers/AccountController.cs
@@ -0,0 +1,118 @@
+﻿using ImportExportManagement_API.Models;
+using ImportExportManagementAPI.Models;
+using ImportExportManagementAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ImportExportManagementAPI.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        AccountRepository _repo;
+        public AccountController()
+        {
+            _repo = new AccountRepository();
+        }
+
+        // GET: api/Partners
+        [HttpGet]
+        public ActionResult<List<Account>> GetAccounts([FromQuery] AccountStatus status)
+        {
+            return _repo.GetAccounts(status);
+        }
+
+
+        // GET: api/Partners/5
+        [HttpGet("account")]
+        public async Task<ActionResult<Account>> GetAccount([FromQuery]string username)
+        {
+            var account = await _repo.GetAccount(username);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return account;
+        }
+
+        // PUT: api/Partners/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut()]
+        public async Task<IActionResult> PutAccount(string username, Account account)
+        {
+            if (username != account.Username)
+            {
+                return BadRequest();
+            }
+
+            _repo.Update(account);
+
+            try
+            {
+                await _repo.SaveAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_repo.Exist(username))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(account);
+        }
+
+        [HttpPost("login")]
+        public  IActionResult CheckLogin([FromBody]Account account)
+        {
+            string username =  _repo.checkLogin(account);
+            if (username != null)
+            {
+                return Ok(username);
+            }
+
+            return NotFound();
+        }
+
+
+        // POST: api/Partners
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Account>> PostAccount(Account account)
+        {
+            _repo.Insert(account);
+            await _repo.SaveAsync();
+
+            return CreatedAtAction("GetAccount", new { username = account.Username }, account);
+        }
+
+        // DELETE: api/Partners/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccount(string username)
+        {
+            var account = await _repo.GetByIDAsync(username);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            _repo.DeleteAccount(account);
+            await _repo.SaveAsync();
+
+            return NoContent();
+        }
+    }
+}
