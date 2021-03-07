d7edf8a9760bfd033c09811a4c2398283bce67d0
Nguyen Van Loi <Loinvse130734@fpt.edu.vn>
2021-03-07 09:20:13 +0700

    Display swagger on iis - LoiNV

--- ImportExportManagementAPI/Startup.cs
+++ ImportExportManagementAPI/Startup.cs
@@ -46,16 +46,13 @@ namespace ImportExportManagementAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ImportExportManagementAPI v1");
-                    c.RoutePrefix = "swagger/v1";
-                });
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ImportExportManagementAPI v1");
+                c.RoutePrefix = String.Empty;
+            });
 
             app.UseHttpsRedirection();
 
