ddb0346a5bac7c4fca6471a3f6b2b3c66b0c4da5
Nguyen Van Loi <Loinvse130734@fpt.edu.vn>
2021-03-11 11:27:45 +0700

    Add Time - LoiNV

--- ImportExportManagementAPI/Models/Schedule.cs
+++ ImportExportManagementAPI/Models/Schedule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImportExportManagementAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,6 +20,9 @@ namespace ImportExportManagement_API.Models
 
         public int GoodsId { get; set; }
         public Goods Goods { get; set; }
+
+        public int TimeTemplateItemId { get; set; }
+        public TimeTemplateItem TimeTemplateItem { get; set; }
 
     }
 }
