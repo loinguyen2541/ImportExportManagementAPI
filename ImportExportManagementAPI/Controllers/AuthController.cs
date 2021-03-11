6e07bbaca2bf4bd98a9e081969f7c23e870260cd
KhanhBDB <khanhbdbse130392@fpt.edu.vn>
2021-03-11 11:25:34 +0700

    KhanhBDB - add authentication and authorize

--- ImportExportManagementAPI/Controllers/AuthController.cs
+++ ImportExportManagementAPI/Controllers/AuthController.cs
@@ -1,15 +1,63 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ImportExportManagement_API.Models;
+using ImportExportManagementAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace ImportExportManagementAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/authentications")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _config; //to get ke, isssuer from appsetting
+        AccountRepository _repo;
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+            _repo = new AccountRepository();
+        }
+
+        [HttpPost("token")]
+        public async Task<ActionResult> Login(Account accountToLogin)
+        {
+            //check username, password
+            Account newAccount = await _repo.Login(accountToLogin);
+            if (newAccount.Username == "admin" && newAccount.Password == "admin")
+            {
+                //save user infor into session
+                
+                //generate token and return it
+                var tokenHandler = new JwtSecurityTokenHandler();
+                //add claims
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, newAccount.Username));
+                claims.Add(new Claim(ClaimTypes.Email, newAccount.Password));
+                claims.Add(new Claim(ClaimTypes.Role, newAccount.RoleId.ToString()));
+
+                //create credential
+                var secretKey = Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]);
+                var symmectricSecurityKey = new SymmetricSecurityKey(secretKey);
+                var credentials = new SigningCredentials(symmectricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+                //create token by handler
+                var access_token = new JwtSecurityToken(
+                    issuer: _config["JwtSettings:Issuer"],
+                    audience: _config["JwtSettings:Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: credentials
+                    );
+                return Ok(tokenHandler.WriteToken(access_token));
+            }
+            return BadRequest("Invalid login");
+        }
     }
 }
--- ImportExportManagementAPI/Startup.cs
+++ ImportExportManagementAPI/Startup.cs
@@ -1,3 +1,5 @@
+using ImportExportManagementAPI.Helper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,11 +8,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ImportExportManagementAPI
@@ -27,6 +31,53 @@ namespace ImportExportManagementAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ////add authentication
+            //services.AddAuthentication(authOptions => {
+            //    authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    authOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).
+            //AddJwtBearer(jwtOptions => {
+            //    jwtOptions.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateAudience = true,
+            //        ValidAudience = Configuration["JwtSettings:Audience"],
+            //        ValidateIssuer = true,
+            //        ValidIssuer = Configuration["JwtSettings:Issuer"],
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSettings:Key"])),
+            //        ValidateLifetime = true
+            //    };
+            //});
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            var validIssuer = appSettings.Issuer;
+            var validAudience = appSettings.Audience;
+            //add authentication
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateAudience = true,
+                    ValidAudience = validAudience,
+                    ValidateIssuer = true,
+                    ValidIssuer = validIssuer,
+                };
+            });
+
+
             services.AddControllers().AddNewtonsoftJson(options =>
             {
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
@@ -40,9 +91,10 @@ namespace ImportExportManagementAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ImportExportManagementAPI", Version = "v1" });
             });
             services.AddDistributedMemoryCache();
-            services.AddSession(o => {
-                 o.IdleTimeout = TimeSpan.FromSeconds(30 * 60);
-             });
+            services.AddSession(o =>
+            {
+                o.IdleTimeout = TimeSpan.FromSeconds(30 * 60);
+            });
 
         }
 
@@ -58,16 +110,17 @@ namespace ImportExportManagementAPI
             });
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
+            //here
+            app.UseAuthentication();
             app.UseAuthorization();
-           
+
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-           
+    {
+        endpoints.MapControllers();
+    });
+
         }
     }
 }
