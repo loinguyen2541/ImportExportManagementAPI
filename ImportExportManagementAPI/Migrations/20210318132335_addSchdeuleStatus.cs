3efdb046cd61410759d68cede831018d2eb20313
Nguyen Van Loi <Loinvse130734@fpt.edu.vn>
2021-03-18 20:24:40 +0700

    add schedule status - LoiNV

---
+++ ImportExportManagementAPI/Migrations/20210318132335_addSchdeuleStatus.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImportExportManagementAPI.Migrations
+{
+    public partial class addSchdeuleStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ScheduleStatus",
+                table: "Schedule",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScheduleStatus",
+                table: "Schedule");
+        }
+    }
+}
