c87a8c0c9b67dc9668cf1b744d91785e5763745d
thanhtan290598 <77422806+thanhtan290598@users.noreply.github.com>
2021-04-15 22:52:08 +0700

    update include partner in notification repo

--- ImportExportManagementAPI/Repositories/NotificationRepository.cs
+++ ImportExportManagementAPI/Repositories/NotificationRepository.cs
@@ -29,7 +29,7 @@ namespace ImportExportManagementAPI.Repositories
         {
             Pagination<Notification> listNotification = new Pagination<Notification>();
             IQueryable<Notification> rawData = null;
-            rawData = _dbSet.Include(p => p.Transaction).OrderBy(n => n.StatusPartner).OrderByDescending(n => n.CreatedDate).Where(n => n.PartnerId == partnerId && n.StatusPartner != NotificationStatus.Unavailable);
+            rawData = _dbSet.Include(p => p.Transaction).ThenInclude(p => p.Partner).OrderBy(n => n.StatusPartner).OrderByDescending(n => n.CreatedDate).Where(n => n.PartnerId == partnerId && n.StatusPartner != NotificationStatus.Unavailable);
             listNotification = await DoPaging(paging, rawData);
             return listNotification;
         }
