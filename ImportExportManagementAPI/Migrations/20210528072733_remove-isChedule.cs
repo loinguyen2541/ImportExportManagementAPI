bca684f139f2791c57c5f6c987c692a1a13460b1
loinguyen.hg.n <loinguyen.hg.n@gmail.com>
2021-05-28 14:28:06 +0700

    fixx schedule

---
+++ ImportExportManagementAPI/Migrations/20210528072733_remove-isChedule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImportExportManagementAPI.Migrations
+{
+    public partial class removeisChedule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsScheduled",
+                table: "Transaction");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsScheduled",
+                table: "Transaction",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
